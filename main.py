c8495a7c006126ef68ce02f473abea52ac17b149
Kerwin <me@zhanghuanglong.com>
2023-02-19 21:47:55 +0800

    request

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, Response, jsonify, render_template
+import sys
+from ali import sign
+
+
+server = Flask(__name__)
+
+
+@server.route('/alisign', methods=['GET'])
+def aliSign():
+    userId = request.args.get('userid')
+    deviceId = request.args.get('deviceid')
+    jwt = request.args.get('jwt')
+    result = {}
+    result["sign"] = sign(deviceId, userId, jwt)
+    return jsonify(result)
+
+
+if __name__ == '__main__':
+    port = 9099
+    if (len(sys.argv) > 1):
+        port = sys.argv[1]
+    server.run(host='0.0.0.0', port=port)
